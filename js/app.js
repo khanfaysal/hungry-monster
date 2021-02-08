4568ab177b6daab3cd6131a601b3d83150683667
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-08 22:52:58 +0600

    almost done code

--- js/app.js
+++ js/app.js
@@ -1,14 +1,99 @@
 
 // search meal js code
+'use strict';
 const searchMeal = () => {
   const searchMealText = document.getElementById('searchMealArea').value;
-   const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchMealText}`
+   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchMealText}
+   `
    fetch(url)
-   .then(res => res.json())
+   .then(res =>res.json())
    .then(data =>{
-    console.log(data)
-   })
-}
+    showMealUi(data.meals)
+   });
+   .catch(error => alert('please input valid information'))
+  };
+
+  const showMealUi = meal => {
+    const mealDivision = document.getElementById('mealItems');
+    mealDivision.innerHTML = '';
+    meal.forEach(mealArray => {
+        const mealsDiv = document.createElement('div');
+        mealsDiv.className = 'col-md-4';
+        mealsDiv.innerHTML = `
+            <div onclick="mealItemIngredint()"; class="border bg-white text-center" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <img class="img-fluid" src="${mealArray.strMealThumb}" alt="mealImg">
+                <h2 class="h4 p-2">${mealArray.strMeal}</h2>
+            </div>
+        `;
+
+        mealDivision.appendChild(mealsDiv);
+    });
+};
+
+const foodIngredient = () => {
+  const url =` https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          //console.log(data);
+          //displayMeal(data.meals);
+          ingredientInfo(data.meals);
+      });
+};
+
+const ingredientInfo = ingredient => {
+  const infoDiv = document.getElementById('ingredientDetails');
+  ingredient.forEach(element => {
+      infoDiv.innerHTML = `
+      <img class="img-fluid" src="${element.strMealThumb}" alt="" />
+      <h2 class="h4 p-2">${element.strIngredient1}</h2>
+      `;
+
+      // console.log(element);
+  });
+};
+
+const mealItemIngredint = () => {
+  const url =` https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+  fetch(url)
+      .then(response => response.json())
+      .then(data => {
+          //console.log(data);
+          //displayMeal(data.meals);
+          mealInformation(data.meals);
+      });
+};
+
+const mealInformation = ingredient => {
+  const infoDiv = document.getElementById('allInfoItem');
+  ingredient.forEach(element => {
+      infoDiv.innerHTML = `
+      <img class="img-fluid" src="${element.strMealThumb}" alt="" />
+      <h2 class="h4 p-2">${element.strIngredient1}</h2>
+      `;
+
+      console.log(element);
+  });
+};
+
+// end js code
+
+  //  const showMealUi = meal =>{
+  //    const mealDivision = document.getElementById('mealItems');
+  //    mealDivision.innerHTML = '';
+  //    meal.forEach(mealArray => {
+  //      const mealDivisionArea = document.getElementById('div');
+  //     //  mealDivisionArea.className = "col-md-3";
+  //      mealDivisionArea.innerHTML=`
+  //           <div>
+  //               <img src="" alt="" >
+  //               <h3>fghgdhd</h2>
+  //           </div>
+  //      `;
+  //      mealDivision.appendChild(mealDivisionArea);
+  //    });
+  //  }; 
+
 // const displayMeal = meals =>{
 //   meals.forEach(food =>){
 //     console.log(food);
