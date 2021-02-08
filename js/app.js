73edd9a0e8ef5c1b5d37fa35e3d8ab887afb58f3
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-08 11:01:18 +0600

    new code for hungry monster

--- js/app.js
+++ js/app.js
@@ -1,26 +1,31 @@
 
-
   const displayMealsDetail = () =>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
     .then(res => res.json())
     .then(data => { 
         showMealInfo(data)
     }) 
+    .catch(error => alert('Please input valid serach.'))
   }
   const showMealInfo = meals =>{
   const mealDiv = document.getElementById('mealUiArea');
+  mealDiv.className = "mealsItem onclick='displayMealDetail('meals')'";
   mealDiv.innerHTML = `
-  <img src="${meals.meals[0].strMealThumb}">
-  <h1>${meals.meals[0].strMeal}</h1>
+  <img class = "mealImg" src="${meals.meals[0].strMealThumb}">
+  <h1 class = "mealHeading">${meals.meals[0].strMeal}</h1>
 `
    }
    displayMealsDetail();
 
-const mealsItem = document.getElementById('mealsItem');
+ /* const mealsItem = document.getElementById('mealsItem');
    function createMealItem(){
     mealsItem.innerHTML = <div class = "item">
       <img src = "" alt =""></img>
       <h3></h3>
-    </div>
+    </div>jhjfgf
    }
-   createMealItem();
+   createMealItem();  */
+
+   const displayMealDetail = meal => {
+    console.log(object);
+   }
