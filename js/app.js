d3fc9f2a38c392a71ec026da3253e2af6607b7c6
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-09 12:43:20 +0600

    final code finish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-// search meal js code
+// search meal js code start
 'use strict';
 const searchMeal = () => {
   const searchMealText = document.getElementById('searchMealArea').value;
@@ -10,7 +10,7 @@ const searchMeal = () => {
    .then(data =>{
     showMealUi(data.meals)
    })
-   .catch(error => alert('Please try again valid keyword.'))
+    .catch(error => alert('Please try again valid keyword.'))
   };
 
   const showMealUi = meal => {
@@ -20,7 +20,7 @@ const searchMeal = () => {
         const mealsDiv = document.createElement('div');
         mealsDiv.className = 'col-md-4';
         mealsDiv.innerHTML = `
-            <div onclick="mealItemIngredint()"; class="border bg-white text-center" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            <div onclick="mealItemIngredint('${mealArray.idMeal}')" class="border bg-white text-center" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 <img class="img-fluid" src="${mealArray.strMealThumb}" alt="mealImg">
                 <h2 class="h4 p-2">${mealArray.strMeal}</h2>
             </div>
@@ -29,84 +29,29 @@ const searchMeal = () => {
         mealDivision.appendChild(mealsDiv);
     });
 };
-const mealItemIngredint = () => {
-  const url =` https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+const mealItemIngredint = (itemparameter) => {
+  const url =` https://www.themealdb.com/api/json/v1/1/lookup.php?i=${itemparameter}`
   fetch(url)
-      .then(response => response.json())
+      .then(res => res.json())
       .then(data => {
-          //console.log(data);
-          //displayMeal(data.meals);
           mealInformation(data.meals);
       });
 };
 
 const mealInformation = ingredient => {
-  const infoDiv = document.getElementById('ingredientDetails');
+  const mealInformationDiv = document.getElementById('ingredientDetails');
   ingredient.forEach(element => {
-      infoDiv.innerHTML = `
-      <img class="img-fluid" src="${element.strMealThumb}" alt="">
+      mealInformationDiv.innerHTML = `
+      <img src="${element.strMealThumb}" class="img-fluid" alt="itemImg">
       <li>${element.strIngredient1}</li>
       <li>${element.strIngredient2}</li>
       <li>${element.strIngredient3}</li>
       <li>${element.strIngredient4}</li>
       <li>${element.strIngredient5}</li>
       <li>${element.strIngredient6}</li>
-      <li>${element.strIngredient}</li>
+      <li>${element.strIngredient7}</li>
       `;
   });
 };
 
 // end js code
-
-  //  const showMealUi = meal =>{
-  //    const mealDivision = document.getElementById('mealItems');
-  //    mealDivision.innerHTML = '';
-  //    meal.forEach(mealArray => {
-  //      const mealDivisionArea = document.getElementById('div');
-  //     //  mealDivisionArea.className = "col-md-3";
-  //      mealDivisionArea.innerHTML=`
-  //           <div>
-  //               <img src="" alt="" >
-  //               <h3>fghgdhd</h2>
-  //           </div>
-  //      `;
-  //      mealDivision.appendChild(mealDivisionArea);
-  //    });
-  //  }; 
-
-// const displayMeal = meals =>{
-//   meals.forEach(food =>){
-//     console.log(food);
-//   }
-// }
-
-//   const displayMeals = () =>{
-//     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=beef')
-//     .then(res => res.json())
-//     .then(data => { 
-//         showMealInfo(data)
-//     }) 
-//     .catch(error => alert('Please input valid serach.'))
-//   }
-//   const showMealInfo = meals =>{
-//   const mealDiv = document.getElementById('mealUiArea');
-//   mealDiv.className = "mealsItem onclick='displayMealDetail('meals')'";
-//   mealDiv.innerHTML = `
-//   <img class = "mealImg" src="${meals.meals[0].strMealThumb}">
-//   <h1 class = "mealHeading">${meals.meals[0].strMeal}</h1>
-// `
-//    }
-//    displayMeals();
-
-//   const mealsItem = document.getElementById('mealsItem');
-//    function createMealItem(){
-//     mealsItem.innerHTML = <div class = "item">
-//       <img src = "" alt =""></img>
-//       <h3></h3>
-//     </div>
-//    }
-//    createMealItem();
-
-//    /* const displayMealDetail = meal => {
-//     console.log(object);
-//    } */
